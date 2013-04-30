0a71ff5cb911c7a6bd357b5d12f780377b573c7e
vpryst <vpryst@IF030.softservecom.com>
2013-04-30 18:57:50 +0300

    Add tests to project

---
+++ src/test/java/org/vpryst/downloadLinkTest/FilePropertyManagerTest.java
@@ -0,0 +1,9 @@
+package org.vpryst.downloadLinkTest;
+
+import org.testng.annotations.Test;
+
+public class FilePropertyManagerTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ src/test/java/org/vpryst/downloadLinkTest/MainTest.java
@@ -0,0 +1,11 @@
+package org.vpryst.downloadLinkTest;
+
+import org.testng.annotations.Test;
+import org.vpryst.downloadLink.Main;
+
+public class MainTest {
+  @Test
+  public void CheckMain() {
+      Main.main(null);
+  }
+}
